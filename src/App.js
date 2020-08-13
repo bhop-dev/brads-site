8ee01e1acbd8682d033db4c37d626cf8872f5319
bhop-dev <bhopkins353@gmail.com>
2020-08-12 21:04:52 -0500

    first commit ever

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Brad's Site</h1>
+        <h4>Welcome to the site</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
